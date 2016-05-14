e1f468785f9b0296419872c43014cbe00aeb898e
Jhoan Munoz <jhoan.munoz@correounivalle.edu.co>
2016-05-14 14:42:25 -0500

    Borrar codigo muerto, agregar Javadoc a Control.java, agregar archivo de prueba

--- Control.java
+++ Control.java
@@ -1,21 +1,20 @@
 import javax.swing.JOptionPane;
 
+/**
+ * Clase de estructuras de control.
+ *
+ * @author Jhoan Munoz
+ */
 public class Control
 {
 	public void testForArray()
 	{
-		//int myArray[] = new int[5];
 		int myArray[] = {1, 2, 3, 4, 5};
 		
 		for (int number : myArray)
 		{
 			JOptionPane.showMessageDialog(null, number);
 		}
-		
-		/*for (int aux = 0; aux < myArray.length; aux++)
-		{
-			JOptionPane.showMessageDialog(null, myArray[aux]);
-		}*/
 	}
 
 	public void testFor(int value)
@@ -69,17 +68,6 @@ public class Control
 	public String  isUnderAge(int age)
 	{
 		return age >= 18 ? "it's not under age" : "it's under age";
-		/*String validation;
-		if (age >= 18)
-		{
-			validation = "it's not under age";
-		}
-		else
-		{
-			validation = "it's under age";
-		}
-		
-		return validation;*/
 	}
 
 	public void testElseIf(int value)
--- PruebaControl.java
+++ PruebaControl.java
@@ -4,12 +4,7 @@ public class PruebaControl
 {
 	public static void main(String args[])
 	{
-		//int value = Integer.parseInt(JOptionPane.showInputDialog("value"));
 		Control control = new Control();
-		//control.testElseIf(value);
-		//control.testSwitch(JOptionPane.showInputDialog("day"));
-	
-		//JOptionPane.showMessageDialog(null, control.isUnderAge(value));
 		control.testForArray();
 	}
 }
